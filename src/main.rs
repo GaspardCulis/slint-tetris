40745ea3688a952665eee2861994b9cbcb3904dc
GaspardCulis <gaspard.culis@gmail.com>
2023-04-25 22:16:58 +0200

    Moved GameGrid to its own component

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,7 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.set_grid_size(Size { height: 20, width: 10 });
-
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });
 
     ui.run()
 }
