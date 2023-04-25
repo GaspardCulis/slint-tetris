e55b04aaa7e6ae09d5c7cad9caf34d8de2157917
GaspardCulis <gaspard.culis@gmail.com>
2023-04-25 16:37:26 +0200

    Started working on the UI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use slint::PhysicalSize;
+
 mod pieces;
 mod game;
 
@@ -5,6 +7,8 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
+    ui.set_grid_size(Size { height: 20, width: 10 });
 
     let ui_handle = ui.as_weak();
     ui.on_request_increase_value(move || {
