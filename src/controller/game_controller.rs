749ffbacd05c9d8bb12fd83ec1e574d55e906a33
GaspardCulis <gaspard.culis@gmail.com>
2023-04-26 14:10:33 +0200

    update_ui should work

--- src/controller/game_controller.rs
+++ src/controller/game_controller.rs
@@ -3,6 +3,8 @@ use std::time::Duration;
 use slint::*;
 
 pub fn setup(window: &AppWindow, game: Game) -> Timer {
+    window.global::<GameGridAdapter>().set_grid_size(Size { height: Game::GRID_HEIGHT.into(), width: Game::GRID_WIDTH.into() });
+
     let update_timer = Timer::default();
     update_timer.start(TimerMode::Repeated, Duration::from_millis(40), {
         let weak_window = window.as_weak();
@@ -16,23 +18,24 @@ pub fn setup(window: &AppWindow, game: Game) -> Timer {
 }
 
 fn update_ui(game_grid_adapter: &GameGridAdapter, game: &Game) {
-    for (i, row) in game.get_grid().iter().enumerate() {
-        game_grid_adapter.get_grid().set_row_data(i, ModelRc<slint::Color>::new([]));
-        for (j, cell) in row.iter().enumerate() {
-            game_grid_adapter.get_grid().row_data(i).insert(ModelRc::<slint::Color>::from(slint::Color{red: 0, green: 0, blue: 0, alpha: 0}));
+    let vec = VecModel::<VecModel<slint::Color>>::default();
+    for i in 0..Game::GRID_HEIGHT {
+        let row = VecModel::<Color>::default();
+        for j in 0..Game::GRID_WIDTH {
+            row.insert(j.into(), col2col( game.get_grid()[i as usize][j as usize] ));
         }
     }
 }
 
 fn col2col(color: Option<pieces::Color>) -> slint::Color {
     match color {
-        Some(pieces::Color::CYAN) => slint::Color{red: 0, green: 255, blue: 255, alpha: 255},
-        Some(pieces::Color::BLUE) => slint::Color{red: 0, green: 0, blue: 255, alpha: 255},
-        Some(pieces::Color::ORANGE) => slint::Color{red: 255, green: 165, blue: 0, alpha: 255},
-        Some(pieces::Color::YELLOW) => slint::Color{red: 255, green: 255, blue: 0, alpha: 255},
-        Some(pieces::Color::GREEN) => slint::Color{red: 0, green: 255, blue: 0, alpha: 255},
-        Some(pieces::Color::PURPLE) => slint::Color{red: 128, green: 0, blue: 128, alpha: 255},
-        Some(pieces::Color::RED) => slint::Color{red: 255, green: 0, blue: 0, alpha: 255},
-        None => slint::Color{red: 0, green: 0, blue: 0, alpha: 0}
+        Some(pieces::Color::CYAN) => slint::Color::from_rgb_u8(0, 255, 255), 
+        Some(pieces::Color::BLUE) => slint::Color::from_rgb_u8(0, 0, 255), 
+        Some(pieces::Color::ORANGE) => slint::Color::from_rgb_u8(255, 165, 0),
+        Some(pieces::Color::YELLOW) => slint::Color::from_rgb_u8(255, 255, 0),
+        Some(pieces::Color::GREEN) => slint::Color::from_rgb_u8(0, 255, 0),
+        Some(pieces::Color::PURPLE) => slint::Color::from_rgb_u8(128, 0, 128),
+        Some(pieces::Color::RED) => slint::Color::from_rgb_u8(255, 0, 0),
+        None => slint::Color::from_argb_u8(0, 0, 0, 0)
     }
 }
