08886370ab767dc34f8df7f85562b36554ce1b80
GaspardCulis <gaspard.culis@gmail.com>
2023-04-30 14:51:56 +0200

    Added showcase screenshot

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,20 @@ fn main() -> Result<(), slint::PlatformError> {
     let game = Rc::new(RefCell::new(Game::new()));
 
     let _game_controller = game_controller::setup(&ui, game.clone());
-
-    let game_handle = game.clone();
-    ui.on_key_pressed(move |key_text: SharedString| {
-        let keycode = key_text.as_str().chars().nth(0).unwrap();
-        let mut game = game_handle.borrow_mut();
-        game.handle_input(keycode);
-        
-    });
-
+ 
     let game_handle = game.clone();
     let game_update_timer = Timer::default();
     game_update_timer.start(slint::TimerMode::Repeated, Duration::from_millis(30), {
         move || {
             game_handle.borrow_mut().update();
         }
+    });
+    
+    let game_handle = game.clone();
+    ui.on_key_pressed(move |key_text: SharedString| {
+        let keycode = key_text.as_str().chars().nth(0).unwrap();
+        let mut game = game_handle.borrow_mut();
+        game.handle_input(keycode);
     });
 
     ui.run()
